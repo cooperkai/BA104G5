8157bf249cd7b0944b0ec7d002b429fa4417f815
cooperkai <eatkaikai@gmail.com>
2017-11-17 18:22:10 +0800

    controller

---
+++ src/com/article/controller/ArticleServlet.java
@@ -0,0 +1,20 @@
+package com.article.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class ArticleServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		doPost(req, res);
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+	}
+
+}
--- src/com/article/model/ArticleService.java
+++ src/com/article/model/ArticleService.java
@@ -1,39 +1,52 @@
 package com.article.model;
 
-import java.sql.Date;
 import java.sql.Timestamp;
+import java.util.List;
 
 public class ArticleService {
-	
+
 	private ArticleDAO_interface dao;
-	public ArticleService (){
+
+	public ArticleService() {
 		dao = new ArticleDAO();
 	}
-	
-	private String article_no;
-	private String rtr_no;
-	private String article_body;
-	private Timestamp post_date;
-	private Date update_date;
-	private String article_state;
-	
-	
-"INSERT INTO Article (Rtr_No, Article_body, Post_date, Article_State) VALUES( ?, ?, ?, ?)";
-"UPDATE Article SET Rtr_No=?, Article_body=?, Post_date=?, Article_State=? WHERE Article_No = ?";
-"SELECT Article_No, Rtr_No, Article_body, to_char(Post_Date, 'yyyy-mm-dd hh:mi:ss')Post_Date, Update_date, Article_State FROM Article WHERE Article_No = ?";
-"SELECT Article_No, Rtr_No, Article_body, to_char(Post_Date, 'yyyy-mm-dd hh:mi:ss')Post_Date, Update_date, Article_State FROM Article ORDER BY Article_No";
 
-"SELECT * FROM Article ORDER BY Post_Date DESC";// 查詢發布時間排序
-	
-	
-	
-	
-	public ArticleVO add(String rtr_no, String article_body, Timestamp post_date, String article_state){
+	// 新增
+	public ArticleVO add(String rtr_no, String article_body, Timestamp post_date, String article_state) {
 		ArticleVO articleVO = new ArticleVO();
-		
-		
-		return null;
-		
+		articleVO.setRtr_no(rtr_no);
+		articleVO.setArticle_body(article_body);
+		articleVO.setPost_date(post_date);
+		articleVO.setArticle_state(article_state);
+		dao.insert(articleVO);
+		return articleVO;
 	}
 
+	// 修改
+	public ArticleVO update(String article_no, String rtr_no, String article_body, Timestamp post_date,
+			String article_state) {
+		ArticleVO articleVO = new ArticleVO();
+		articleVO.setArticle_no(article_no);
+		articleVO.setRtr_no(rtr_no);
+		articleVO.setArticle_body(article_body);
+		articleVO.setPost_date(post_date);
+		articleVO.setArticle_state(article_state);
+		dao.update(articleVO);
+		return articleVO;
+	}
+
+	// 查單一
+	public ArticleVO getOne(String articles_no) {
+		return dao.findByPrimaryKey(articles_no);
+	}
+
+	// 查全部
+	public List<ArticleVO> getAll() {
+		return dao.getAll();
+	}
+
+	// 查詢全部照時間排序
+	public List<ArticleVO> getAllByTime() {
+		return dao.getAllByTime();
+	}
 }
